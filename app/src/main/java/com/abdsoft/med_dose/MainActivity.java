bf170f85da623b4f6a6b8478e0038b8f4ab27b5b
Abdullah Deshmukh <abdullahzakir99@gmail.com>
2019-10-07 20:01:36 +0530

    Add new BottomNavigationActivity as HomeActivity

--- app/src/main/java/com/abdsoft/med_dose/MainActivity.java
+++ app/src/main/java/com/abdsoft/med_dose/MainActivity.java
@@ -2,7 +2,11 @@ package com.abdsoft.med_dose;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Objects.requireNonNull(getSupportActionBar()).hide();
+        }
+
+        Thread splash = new Thread(){
+            public void run()
+            {
+                try
+                {
+                    sleep(1500);
+                    Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        };
+        splash.start();
     }
 }
---
+++ app/src/main/java/com/abdsoft/med_dose/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,19 @@
+package com.abdsoft.med_dose.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public NotificationsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is notifications fragment");
+    }
+
+    LiveData<String> getText() {
+        return mText;
+    }
+}
