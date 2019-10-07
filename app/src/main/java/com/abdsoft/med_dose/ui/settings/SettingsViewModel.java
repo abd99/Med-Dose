5f78e74f394863bc646e374f640cc6b74ef17cac
Abdullah Deshmukh <abdullahzakir99@gmail.com>
2019-10-07 20:17:15 +0530

    Change Notifications fragment to Settings fragment in the entire project

--- app/src/main/java/com/abdsoft/med_dose/ui/settings/SettingsViewModel.java
+++ app/src/main/java/com/abdsoft/med_dose/ui/settings/SettingsViewModel.java
@@ -1,14 +1,14 @@
-package com.abdsoft.med_dose.ui.notifications;
+package com.abdsoft.med_dose.ui.settings;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class SettingsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public SettingsViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is notifications fragment");
     }
